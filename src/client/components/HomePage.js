5c0e451e2e6c32e485b7aee89575711b99403397
hamed <h.karachi@gmail.com>
2021-01-16 21:42:03 +0100

    test

--- src/client/components/HomePage.js
+++ src/client/components/HomePage.js
@@ -15,10 +15,10 @@ export function HomePage() {
         <NavigateBar className="NavbarItems" />
 
         <p className="mainPage">
-          Here is the amazing place to provide you an opportunity to share your
-          meals with the others. We have a very luxux and beautiful place in the
-          city center with every thing you need for making your meals and host
-          your customers. And also here is a place for you as a customer to
+          Here is the very amazing place to provide you an opportunity to share
+          your meals with the others. We have a very luxux and beautiful place
+          in the city center with every thing you need for making your meals and
+          host your customers. And also here is a place for you as a customer to
           select what you want completely diffenet than the normal restaurants.
           Enjoy...
         </p>
